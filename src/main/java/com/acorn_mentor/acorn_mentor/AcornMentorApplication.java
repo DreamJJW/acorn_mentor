0b9c3645a309e0387c8c07e170f974d2678cc016
dreamjjw <chiwoojw@gmail.com>
2023-05-30 12:34:24 +0900

    #6- Spring initialize
    스프링부트를 초기화하였다.
    * Spring Web
    * Actuator
    * Devtools
    * Lombok

---
+++ src/main/java/com/acorn_mentor/acorn_mentor/AcornMentorApplication.java
@@ -0,0 +1,13 @@
+package com.acorn_mentor.acorn_mentor;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AcornMentorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AcornMentorApplication.class, args);
+	}
+
+}
