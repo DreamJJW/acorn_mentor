10699f5728f6a7af9e60760261a48472bd874442
dreamjjw <chiwoojw@gmail.com>
2023-06-10 01:52:31 +0900

    #12 - 게시글 뷰 테스트 작성
    게시글 뷰 테스트 작성

---
+++ src/main/java/com/acorn_mentor/acorn_mentor/controller/PostController.java
@@ -0,0 +1,10 @@
+package com.acorn_mentor.acorn_mentor.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@RequestMapping("/posts")
+@Controller
+public class PostController {
+
+}
---
+++ src/test/java/com/acorn_mentor/acorn_mentor/controller/PostControllerTest.java
@@ -0,0 +1,74 @@
+package com.acorn_mentor.acorn_mentor.controller;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.http.MediaType;
+import org.springframework.test.web.servlet.MockMvc;
+
+import static org.junit.jupiter.api.Assertions.*;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
+
+@DisplayName("View 컨트롤러 - 게시글")
+@WebMvcTest(PostControllerTest.class) // PostController 만 읽어들이고 테스트 실행.
+class PostControllerTest {
+
+    private final MockMvc mvc;
+
+    public PostControllerTest(@Autowired MockMvc mvc) {
+        this.mvc = mvc;
+    }
+
+    @DisplayName("[view][GET] 게시글 리스트 (게시판) 페이지 - 정상 호출")
+    @Test
+    public void post_list_view() throws Exception {
+        // given
+
+        // when & then
+        mvc.perform(get("/posts"))
+                .andExpect(status().isOk())
+                .andExpect(content().contentType(MediaType.TEXT_HTML))
+                .andExpect(model().attributeExists("posts"));
+
+    }
+
+    @DisplayName("[view][GET] 게시글 특정 페이지 - 정상 호출")
+    @Test
+    public void specific_post_view() throws Exception {
+        // given
+
+        // when & then
+        mvc.perform(get("/posts/1"))
+                .andExpect(status().isOk())
+                .andExpect(content().contentType(MediaType.TEXT_HTML))
+                .andExpect(model().attributeExists("post"));
+
+    }
+
+    @DisplayName("[view][GET] 게시글 검색 전용 페이지 - 정상 호출")
+    @Test
+    public void post_search() throws Exception {
+        // given
+
+        // when & then
+        mvc.perform(get("/posts/search"))
+                .andExpect(status().isOk())
+                .andExpect(content().contentType(MediaType.TEXT_HTML));
+        
+    }
+
+    @DisplayName("[view][GET] 게시글 해시태그 검색 페이지 - 정상 호출")
+    @Test
+    public void hashtag_search() throws Exception {
+        // given
+
+        // when & then
+        mvc.perform(get("/posts/search-hashtag"))
+                .andExpect(status().isOk())
+                .andExpect(content().contentType(MediaType.TEXT_HTML));
+
+    }
+
+}
