43c7592d25ae81b9d2c887df595537af410bbc39
Jiwoo <chiwoojw@gmail.com>
2023-06-10 15:08:04 +0900

    #12 - 컨트롤러 및 테스트 기타 리팩토링
    컨트롤러 및 테스트 기타 리팩토링

--- src/main/java/com/acorn_mentor/acorn_mentor/controller/PostController.java
+++ src/main/java/com/acorn_mentor/acorn_mentor/controller/PostController.java
@@ -1,10 +1,9 @@
 package com.acorn_mentor.acorn_mentor.controller;
 
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
 import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 
 import java.util.List;
 
@@ -13,7 +12,15 @@ public class PostController {
     @GetMapping("/index")
     public String posts(ModelMap modelMap) {
         modelMap.addAttribute("posts", List.of());
-        return "index";
+        return "posts/index";
     }
+
+    @GetMapping("posts/{postId}")
+    public String post(@PathVariable Long postId, ModelMap modelMap) {
+        modelMap.addAttribute("post", null);
+        modelMap.addAttribute("comments", List.of());
+        return "posts/detail";
+    }
+
 
 }
