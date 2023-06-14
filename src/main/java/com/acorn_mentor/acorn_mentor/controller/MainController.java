776149384e3ec4416f8cc6adf6fcf472487781da
dreamjjw <chiwoojw@gmail.com>
2023-06-14 21:23:29 +0900

    # 홈페이지로 포워딩 추가
    MainController와 MainControllerTest에 홈 버튼
    포워딩 추가

---
+++ src/main/java/com/acorn_mentor/acorn_mentor/controller/MainController.java
@@ -0,0 +1,13 @@
+package com.acorn_mentor.acorn_mentor.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class MainController {
+
+    @GetMapping("/")
+    public String root() {
+        return "forward:/posts";
+    }
+}
